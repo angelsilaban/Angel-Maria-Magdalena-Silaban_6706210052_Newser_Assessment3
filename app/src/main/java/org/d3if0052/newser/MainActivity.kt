8b0a854afdd099bb0220e3f2cef39e1824ecfee0
Angel Maria Magdalena Silaban <magdalenangela20@gmail.com>
2023-03-29 17:22:55 +0700

    home page NEWSER (butuh perbaikan)

--- app/src/main/java/org/d3if0052/newser/MainActivity.kt
+++ app/src/main/java/org/d3if0052/newser/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.d3if0052.newser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            object : Thread() {
+                override fun run() {
+                    sleep(3000)
+                    startActivity(
+                        Intent(
+                            this@MainActivity,
+                            HomePageActivity::class.java
+                        )
+                    )
+                    finish()
+                }
+            }.start()
+        } catch (ex: Exception) {
+            Toast.makeText(
+                this@MainActivity,
+                "Failed to start app!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
