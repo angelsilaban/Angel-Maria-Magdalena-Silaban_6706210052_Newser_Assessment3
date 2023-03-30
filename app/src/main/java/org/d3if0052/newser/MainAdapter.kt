4e9a377b437eaa58a51867297593edaa34de2c86
Angel Maria Magdalena Silaban <magdalenangela20@gmail.com>
2023-03-30 16:28:44 +0700

    search view dan main adapter NEWSER (butuh perbaikan)

--- app/src/main/java/org/d3if0052/newser/MainAdapter.kt
+++ app/src/main/java/org/d3if0052/newser/MainAdapter.kt
@@ -3,11 +3,12 @@ package org.d3if0052.newser
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.d3if0052.newser.databinding.ActivityListBeritaBinding
 
 class MainAdapter(private val data: List<Berita>) :
     RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         class ViewHolder(
-            private val binding: ListItemBinding) :
+            private val binding: ActivityListBeritaBinding) :
             RecyclerView.ViewHolder(binding.root) {
 
                 fun bind(berita: Berita) = with(binding) {
@@ -19,7 +20,7 @@ class MainAdapter(private val data: List<Berita>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemBinding.inflate(inflater, parent, false)
+        val binding = ActivityListBeritaBinding.inflate(inflater, parent, false)
         return ViewHolder(binding)
     }
 
