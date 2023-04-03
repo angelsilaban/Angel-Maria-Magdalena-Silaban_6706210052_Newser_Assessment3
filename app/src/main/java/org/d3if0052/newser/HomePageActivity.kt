991575b35b62357abf2973bdd932f22606d36542
Angel Maria Magdalena Silaban <magdalenangela20@gmail.com>
2023-04-03 19:03:43 +0700

    ui error - fixing

--- app/src/main/java/org/d3if0052/newser/HomePageActivity.kt
+++ app/src/main/java/org/d3if0052/newser/HomePageActivity.kt
@@ -3,6 +3,7 @@ package org.d3if0052.newser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.d3if0052.newser.databinding.ActivityHomePageBinding
@@ -11,84 +12,112 @@ import kotlin.collections.ArrayList
 class HomePageActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomePageBinding
+    private var beritalist = arrayListOf<Berita>()
+    private lateinit var beritaAdapter: MainAdapter
+    private val list = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //memanggil class Adapter dengan data yang dimiliki
-        val beritaAdaptar = MainAdapter(getData())
 
-        //melakukan pencarian data dan menghubungkan dengan class Main Adapter
-        binding.search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
+        for (i in beritalist.indices) {
+            list.add(beritalist[i].title)
+        }
 
-                    (binding.recycleViewListBerita.adapter as MainAdapter).filter.filter(query)
+//        beritalist.addAll(getData())
+        beritaAdapter = MainAdapter(getData())
 
-                Log.wtf("AAAAAAA", "request: $query")
-                beritaAdaptar.updateList()
-
-                return false
-            }
-
-            //melakukan penghapusan data dan mereset list dan menghubungkan dengan class Main Adapter
-            override fun onQueryTextChange(newText: String?): Boolean {
-                (binding.recycleViewListBerita.adapter as MainAdapter).filter.filter(newText)
-
-                Log.wtf("AAAAAAA", "request: $newText")
-                beritaAdaptar.updateList()
-
-                if (newText!!.isEmpty() || newText.isBlank()) beritaAdaptar.resetList()
-                return false
-            }
-
-        })
-
-        with(binding.recycleViewListBerita) {
+        with(binding.rvBerita) {
             layoutManager = LinearLayoutManager(this@HomePageActivity)
-            adapter = beritaAdaptar
+            adapter = beritaAdapter
             setHasFixedSize(true)
         }
-
     }
-            // membuat list data dengan arrayList
-            private fun getData(): ArrayList<Berita> {
-                return arrayListOf(
-                    Berita(
-                        "Polisi tangkap 21  tersangka narkoba di Bogor, sabu hingga 0bat keras disita.",
-                        "Newser - 3 jam yang lalu",
-                        R.drawable.image_narkoba
-                    ),
 
-                    Berita(
-                        "Salah satu pulau terpadat di dunia ternyata milik Indonesia.",
-                        "Newser - 5 jam yang lalu",
-                        R.drawable.image_pulau
-                    ),
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
 
-                    Berita(
-                        "Tesla Cybertruck belum meluncur, kaum tajir Indonesia sudah antre mau beli.",
-                        "Newser - 9 jam yang lalu",
-                        R.drawable.image_tesla
-                    ),
+        val menuItem = menu.findItem(R.id.search)
+        val searchView = menuItem.actionView as SearchView
+        performSearching(searchView)
 
-                    Berita(
-                        "Potret Tanaman Bibit Cerdas yang Bantu Tingkatkan Hasil Panen di China.",
-                        "Newser - 14 jam yang lalu",
-                        R.drawable.image_tanamanbibitcerdas
-                    ),
+        return super.onCreateOptionsMenu(menu)
+    }
 
-                    Berita(
-                        "Aksi Protes Massal di Israel Berlanjut.",
-                        "Newser -16 jam yang lalu",
-                        R.drawable.image_protesisrael
-                    ),
+    //melakukan pencarian data dan menghubungkan dengan class Main Adapter
+    private fun performSearching(searchView: SearchView) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                for (i in list.indices) {
+                    Log.wtf("AEAEAE", "onQueryTextSubmit: ${list[i]}")
 
-                    Berita(
-                        "Pesawat Kelima Pelita Air Sudah Datang, untuk Mudik 2023.",
-                        "Newser -18 jam yang lalu",
-                        R.drawable.image_pesawatpelitaair
-                    ),
-                )
+                    if (list[i].contains(query!!)) {
+                        beritaAdapter.filter.filter(query)
+                    }
+                }
+                return false
             }
-        }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                beritaAdapter.filter.filter(p0)
+                return true
+            }
+        })
+    }
+}
+
+// membuat list data dengan arrayList
+private fun getData(): ArrayList<Berita> {
+    return arrayListOf(
+        Berita(
+            "Polisi tangkap 21  tersangka narkoba di Bogor, sabu hingga 0bat keras disita.",
+            "Newser - 3 jam yang lalu",
+            R.drawable.image_narkoba
+        ),
+
+        Berita(
+            "Salah satu pulau terpadat di dunia ternyata milik Indonesia.",
+            "Newser - 5 jam yang lalu",
+            R.drawable.image_pulau
+        ),
+
+        Berita(
+            "Tesla Cybertruck belum meluncur, kaum tajir Indonesia sudah antre mau beli.",
+            "Newser - 9 jam yang lalu",
+            R.drawable.image_tesla
+        ),
+
+        Berita(
+            "Potret Tanaman Bibit Cerdas yang Bantu Tingkatkan Hasil Panen di China.",
+            "Newser - 14 jam yang lalu",
+            R.drawable.image_tanamanbibitcerdas
+        ),
+
+        Berita(
+            "Aksi Protes Massal di Israel Berlanjut.",
+            "Newser -16 jam yang lalu",
+            R.drawable.image_protesisrael
+        ),
+
+        Berita(
+            "Pesawat Kelima Pelita Air Sudah Datang, untuk Mudik 2023.",
+            "Newser -18 jam yang lalu",
+            R.drawable.image_pesawatpelitaair
+        ),
+    )
+}
+//
+//            //melakukan penghapusan data dan mereset list dan menghubungkan dengan class Main Adapter
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                (binding.recycleViewListBerita.adapter as MainAdapter).filter.filter(newText)
+//
+//                Log.wtf("AAAAAAA", "request: $newText")
+//                beritaAdaptar.updateList()
+//
+//                if (newText!!.isEmpty() || newText.isBlank()) beritaAdaptar.resetList()
+//                return false
+//            }
+
+//        })
--- app/src/main/java/org/d3if0052/newser/MainAdapter.kt
+++ app/src/main/java/org/d3if0052/newser/MainAdapter.kt
@@ -5,99 +5,161 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import com.bumptech.glide.Glide
 import androidx.recyclerview.widget.RecyclerView
 import org.d3if0052.newser.databinding.ActivityListBeritaBinding
 
-class MainAdapter(private var data: ArrayList<Berita>) :
+class MainAdapter(private var beritalist: ArrayList<Berita>) :
     RecyclerView.Adapter<MainAdapter.ViewHolder>(), Filterable {
-    private var dataFiltered: ArrayList<Berita>? = null
-    private var tempList = arrayListOf<Berita>()
 
-    // mengupdate list
-    fun updateList() {
-        tempList.addAll(data)
-        Log.wtf("AAAAAA", "resetList: $tempList")
-
-        if (dataFiltered!!.isNotEmpty()) {
-            data.clear()
-            data.addAll(dataFiltered!!)
-        }
-    }
-
-    //mereset list
-    fun resetList() {
-        data.clear()
-
-        Log.wtf("AAAAAA", "resetList: $tempList")
-        data.addAll(tempList)
-
-    }
-
-    //membuat class viewholder untuk menampilkan informasi yang ada di list_item
     class ViewHolder(
-        private val binding: ActivityListBeritaBinding
+        val binding: ActivityListBeritaBinding
     ) :
         RecyclerView.ViewHolder(binding.root) {
+        fun bind(berita: Berita) {
+            Glide.with(binding.root)
+                .load(berita.image)
+                .into(binding.imageView)
 
-        fun bind(berita: Berita) = with(binding) {
-            titleTextView.text = berita.title
-            descTextView.text = berita.desc
-            imageNarkoba.setImageResource(berita.image)
+            with(binding) {
+                titleTextView.text = berita.title
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ActivityListBeritaBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
-    }
+    var filterableList: ArrayList<Berita> = beritalist
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            ActivityListBeritaBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(data[position])
-    }
+    override fun getItemCount() = filterableList.size
 
-    override fun getItemCount(): Int {
-        return data.size
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(beritalist[position])
 
-    //melakukan menu searchView
     override fun getFilter(): Filter {
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults {
                 val charString = constraint.toString().lowercase()
 
-                if (charString.isEmpty()) dataFiltered = data
-                else {
-                    data.let {
-                        val filtered = arrayListOf<Berita>()
-
-                        for (berita in data) {
-                            if (berita.title.lowercase()
-                                    .contains(charString) || berita.desc.lowercase()
-                                    .contains(charString)
-                            ) {
-                                filtered.add(berita)
+                filterableList = when {
+                    charString.isEmpty() -> beritalist
+                    else -> {
+                        val filteringList = arrayListOf<Berita>()
+                        for (data in beritalist) {
+                            if (data.title.contains(charString, ignoreCase = true)) {
+                                filteringList.add(data)
                             }
                         }
-                        dataFiltered = filtered
+                        filteringList
                     }
                 }
-                Log.wtf("AAAAAA", "performFiltering: $dataFiltered")
 
                 val res = FilterResults()
-                res.values = dataFiltered
-
+                res.values = filterableList
                 return res
             }
+
             //untuk menampilkan hasil data dari searchView
             override fun publishResults(str: CharSequence?, filter: FilterResults?) {
-                dataFiltered =
-                    if (filter?.values == null) arrayListOf()
-                    else filter.values as ArrayList<Berita>
-
-                Log.wtf("test", "publishResults: $dataFiltered")
+                filterableList = filter?.values as ArrayList<Berita>
             }
 
         }
     }
+
+//        fun bind(berita: Berita) = with(binding) {
+//            titleTextView.text = berita.title
+//            descTextView.text = berita.desc
+//            imageNarkoba.setImageResource(berita.image)
+//        }
 }
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val inflater = LayoutInflater.from(parent.context)
+//        val binding = ActivityListBeritaBinding.inflate(inflater, parent, false)
+//        return ViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.bind(data[position])
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return data.size
+//    }
+//
+//    //melakukan menu searchView
+//    override fun getFilter(): Filter {
+//        return object : Filter() {
+//            override fun performFiltering(constraint: CharSequence?): FilterResults {
+//                val charString = constraint.toString().lowercase()
+//
+//                if (charString.isEmpty()) dataFiltered = data
+//                else {
+//                    data.let {
+//                        val filtered = arrayListOf<Berita>()
+//
+//                        for (berita in data) {
+//                            if (berita.title.lowercase()
+//                                    .contains(charString) || berita.desc.lowercase()
+//                                    .contains(charString)
+//                            ) {
+//                                filtered.add(berita)
+//                            }
+//                        }
+//                        dataFiltered = filtered
+//                    }
+//                }
+//                Log.wtf("AAAAAA", "performFiltering: $dataFiltered")
+//
+//                val res = FilterResults()
+//                res.values = dataFiltered
+//
+//                return res
+//            }
+//            //untuk menampilkan hasil data dari searchView
+//            override fun publishResults(str: CharSequence?, filter: FilterResults?) {
+//                dataFiltered =
+//                    if (filter?.values == null) arrayListOf()
+//                    else filter.values as ArrayList<Berita>
+//
+//                Log.wtf("test", "publishResults: $dataFiltered")
+//            }
+//
+//        }
+//    }
+//    private var dataFiltered: ArrayList<Berita>? = null
+//    private val tempList = arrayListOf<Berita>()
+
+//    fun loadData(newData: List<Berita>) {
+//        tempList.apply {
+//            clear()
+//            addAll(newData)
+//        }
+//    }
+
+// mengupdate list
+//    fun updateList() {
+//        tempList.addAll(data)
+//        Log.wtf("AAAAAA", "resetList: $tempList")
+//
+//        if (dataFiltered!!.isNotEmpty()) {
+//            data.clear()
+//            data.addAll(dataFiltered!!)
+//        }
+//    }
+
+//mereset list
+//    fun resetList() {
+//        data.clear()
+//
+//        Log.wtf("AAAAAA", "resetList: $tempList")
+//        data.addAll(tempList)
+//
+//    }
