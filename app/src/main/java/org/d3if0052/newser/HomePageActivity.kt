20bf5554dc3660b597c72d2e6f9b980926abb395
Angel Maria Magdalena Silaban <magdalenangela20@gmail.com>
2023-05-18 01:22:12 +0700

    Penambahan dan pengubahan activity list berita, fragment history, fragment home, dan home page activity

--- app/src/main/java/org/d3if0052/newser/HomePageActivity.kt
+++ app/src/main/java/org/d3if0052/newser/HomePageActivity.kt
@@ -2,17 +2,11 @@ package org.d3if0052.newser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.widget.Toast
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import org.d3if0052.newser.adapter.MainAdapter
 import org.d3if0052.newser.databinding.ActivityHomePageBinding
 import org.d3if0052.newser.fragment.CategoriesFragment
 import org.d3if0052.newser.fragment.HistoryFragment
 import org.d3if0052.newser.fragment.HomeFragment
-import org.d3if0052.newser.fragment.LikeFragment
-import org.d3if0052.newser.model.Berita
 
 
 class HomePageActivity : AppCompatActivity() {
@@ -32,7 +26,6 @@ class HomePageActivity : AppCompatActivity() {
             when(it.itemId) {
                 R.id.ic_beranda -> addFragment(HomeFragment())
                 R.id.ic_categories -> addFragment(CategoriesFragment())
-                R.id.ic_like -> addFragment(LikeFragment())
                 R.id.ic_history -> addFragment(HistoryFragment())
 
                 else->{
